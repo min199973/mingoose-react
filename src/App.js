afb0b9c5ad628640b16f0f7cbb678088157413d1
min199973 <min199973@gmail.com>
2023-04-03 10:16:11 +0900

    React #7.2 ~ #7.10

--- src/App.js
+++ src/App.js
@@ -1,47 +1,17 @@
-import { useEffect, useState } from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState("");
-  const onChange = (event) => {
-    setMoney(event.target.value);
-  };
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
   return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <div>
-          <div>
-            I have <input type="number" value={money} onChange={onChange} /> $
-          </div>
-          <select>
-            {coins.map((coin) => (
-              <option key={coin.id}>
-                {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
-              </option>
-            ))}
-          </select>
-          <div>
-            you can get
-            {coins.map((coin) => (
-              <span key={coin.id}>{money / coin.quotes.USD.price}</span>
-            ))}
-            ;
-          </div>
-        </div>
-      )}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route path="/hello" element={<Home />}></Route>
+        <Route path="/movie/:id" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
